6cdfb778c05115d0e4170ee7da05138fae5af39e
n0r7hw1ng <syncifiedd@gmail.com>
2024-06-18 10:45:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var password = process.env.password;
 var shared_secret = process.env.shared;
 
 var games = [1938090];  // Enter here AppIDs of the needed games
-var status = 1;  // 1 - online, 7 - invisible
+var status = 7;  // 1 - online, 7 - invisible
 
 
 user = new steamUser();
